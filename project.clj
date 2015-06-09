305380b35f8f0bfbb34c19d4b462ef3d621adb5a
Ray Miller <ray@metail.com>
2015-06-09 15:03:58 +0100

    Update version for next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-http-mock "0.2.0"
+(defproject clj-http-mock "0.3.0-SNAPSHOT"
   :description "Mock responses to clj-http requests."
   :url "http://github.com/ray1729/clj-http-mock"
   :license {:name "Eclipse Public License"
