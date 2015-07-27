c148b1ebcd8fe9dceb0f35762cd000ac4d4abbf9
Ray Miller <ray@metail.com>
2015-07-27 11:08:04 +0100

    Update clj-http dependency.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-http-mock "0.3.0-SNAPSHOT"
+(defproject clj-http-mock "0.3.0"
   :description "Mock responses to clj-http requests."
   :url "http://github.com/ray1729/clj-http-mock"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1"]
+                 [clj-http "2.0.0"]
                  [robert/hooke "1.3.0"]
                  [ring/ring-codec "1.0.0"]
                  [medley "0.6.0"]])
