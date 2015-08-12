4dbffb3e3321e8b79449b7da4acefdfcefc67b51
Ray Miller <ray@metail.com>
2015-08-12 12:06:50 +0100

    Allow response methods to specify headers. Optionally coerce body of resource response. Coerce header maps in responses to clj-http.headers/HeaderMap objects.

--- src/clj_http_mock/response.clj
+++ src/clj_http_mock/response.clj
@@ -1,18 +1,31 @@
 (ns clj-http-mock.response
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clj-http.headers]))
+
+(defn coerce-headers
+  [headers]
+  (into (clj-http.headers/header-map) headers))
 
 (defn resource-response
-  [resource-name]
-  (constantly {:status 200 :headers {} :body (slurp (io/resource resource-name))}))
+  [resource-name & {:keys [headers coerce-body] :or {headers {} coerce-body identity}}]
+  (constantly {:status  200
+               :headers (coerce-headers headers)
+               :body    (coerce-body (slurp (io/resource resource-name)))}))
 
 (defn ok-response
-  [body]
-  (constantly {:status 200 :headers {} :body body}))
+  [body & {:keys [headers] :or {headers {}}}]
+  (constantly {:status  200
+               :headers (coerce-headers headers)
+               :body    body}))
 
 (defn redirect-response
-  [location & {:keys [status] :or {status 302}}]
-  (constantly {:status status :headers {"location" location} :body nil}))
+  [location & {:keys [status headers] :or {status 302 headers {}}}]
+  (constantly {:status  status
+               :headers (coerce-headers (merge headers {"location" location}))
+               :body    nil}))
 
 (defn not-found-response
-  []
-  (constantly {:status 404 :headers {} :body ""}))
+  [& {:keys [headers] :or {headers {}}}]
+  (constantly {:status  404
+               :headers (coerce-headers headers)
+               :body    ""}))
